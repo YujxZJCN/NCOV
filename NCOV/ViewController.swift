92cd04f2f7aae0271e4ba172aeaeced75c45c26b
YujxZJCN <yujxzjcn@vip.qq.com>
2020-04-13 13:15:51 +0800

    Initial Commit

---
+++ NCOV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NCOV
+//
+//  Created by 俞佳兴 on 2020/4/13.
+//  Copyright © 2020 yjx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
